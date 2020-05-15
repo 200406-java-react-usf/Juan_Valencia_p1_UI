61bb6319c32bc685d0a106565f9bdf80a3403524
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-14 21:23:20 -0400

    trying to fix flight request

--- src/remote/auth-service.ts
+++ src/remote/auth-service.ts
@@ -2,11 +2,11 @@ import {revaboardsClient} from './revaboards-client';
 
 
 export async function authenticate(username: string, password: string){
-    let response = await revaboardsClient.post('/auth', {username, password});
+    let response = await revaboardsClient.post('http://p1reimbursement-env.eba-2gpgjpzh.us-east-1.elasticbeanstalk.com/auth', {username, password});
     return await response.data;
 }
 
 export async function addNewEmployee(username: string, password: string, firstName: string, lastName: string, email: string, role: string){
-    let response = await revaboardsClient.post('/employees', {username,password,firstName,lastName,email,role})
+    let response = await revaboardsClient.post('http://p1reimbursement-env.eba-2gpgjpzh.us-east-1.elasticbeanstalk.com/employees', {username,password,firstName,lastName,email,role})
     return await response.data;
 }
